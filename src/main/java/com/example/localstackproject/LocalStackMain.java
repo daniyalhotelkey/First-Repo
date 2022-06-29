189b1fe8406349242d42e806af9648ba6dc91840
daniyalhotelkey <daniyal.mateen@hotelkeyapp.com>
2022-06-29 13:07:02 +0500

    first commit

---
+++ src/main/java/com/example/localstackproject/LocalStackMain.java
@@ -0,0 +1,47 @@
+package com.example.localstackproject;
+
+import com.amazonaws.SDKGlobalConfiguration;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.model.*;
+
+import java.util.Arrays;
+
+public class LocalStackMain {
+    public static void main(String[] args) {
+        System.setProperty(SDKGlobalConfiguration.DISABLE_CERT_CHECKING_SYSTEM_PROPERTY,"true");
+        AmazonDynamoDB client =  AmazonDynamoDBClientBuilder.standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(
+                        "http://localhost:4566",
+                        "us-east-2"))
+                .build();
+
+        DynamoDB dynamoDB = new DynamoDB(client);
+        String table_name = "testDynamoDB";
+        try{
+            System.out.println("Creating Table...");
+            Table table = dynamoDB.createTable(table_name,
+                    Arrays.asList(new KeySchemaElement("year", KeyType.HASH),
+                            new KeySchemaElement("title",KeyType.RANGE)),
+                    Arrays.asList(new AttributeDefinition("year", ScalarAttributeType.N),
+                            new AttributeDefinition("title",ScalarAttributeType.S)),
+                    new ProvisionedThroughput(10L,10L));
+
+            System.out.println("Table: " + table_name
+                    + " Status: " + (table.getDescription() != null
+                    ? table.getDescription().getTableStatus():
+                    "Inactive!!"));
+
+            table.waitForActive();
+            System.out.println("Table: " + table_name + " Status: " + table.getDescription().getTableStatus());
+
+        }catch(Exception exp){
+            System.out.println("Operation Failed :(");
+            System.out.println(exp.getMessage());
+
+        }
+    }
+}
---
+++ src/test/java/VerifyDynamoDBTest.java
@@ -0,0 +1,40 @@
+import com.example.localstackproject.LocalStackDynamoDB;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.model.*;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class VerifyDynamoDBTest {
+
+    LocalStackDynamoDB localStackDynamoDB = new LocalStackDynamoDB();
+
+    @Before
+    public void setup(){
+        this.localStackDynamoDB.setDisableSSLVerification(true);
+
+        this.localStackDynamoDB.init("https:<IP/A-Record>:4566", "us-west-2");
+    }
+
+    @Test
+    public void testTableCreate(){
+
+        Assert.assertEquals("Music", this.localStackDynamoDB.createTable(
+                        "Music",
+                        Arrays.asList(new KeySchemaElement("year", KeyType.HASH),
+                                new KeySchemaElement("title", KeyType.RANGE)),
+                        Arrays.asList(new AttributeDefinition("year", ScalarAttributeType.N),
+                                new AttributeDefinition("title", ScalarAttributeType.S)),
+                        new ProvisionedThroughput(10L, 10L))
+                .getTable("Music").getTableName());
+    }
+
+    @Test
+    public void testTableDelete(){
+        Assert.assertNull(this.localStackDynamoDB.deleteTable("Music").getTable("Music"));
+    }
+}
